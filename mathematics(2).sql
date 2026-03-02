24151755b375252ef6f9b52b83cfe05349f5829d
kireeti200105 <kireetibadugu@gmail.com>
2026-03-02 13:46:13 +0530

    Add files via upload

---
+++ mathematics(2).sql
@@ -0,0 +1,53 @@
+-- Find sine value of an angle.
+-- select sin(90*(acos(-1)/180))from dual;
+
+-- Find cosine value of an angle.
+-- select cos(45*(acos(-1)/180)) from dual;
+
+-- Find tangent value.
+-- select tan(30*(acos(-1)/180)) from dual;
+
+-- Convert degrees to radians.
+-- select 90*(acos(-1)/180) from dual;
+
+-- Convert radians to degrees.
+-- select 1*(180/acos(-1)) from dual;
+
+-- Find logarithm (base 10) of a number.
+-- select log(10,100) from dual;
+
+-- Find natural log of a number.
+-- select ln(10) from dual;
+
+
+-- Find square of a number.
+-- select power(5,2) from dual;
+
+-- Find cube of a number.
+-- select power(5,3) from dual;
+
+-- Calculate factorial of a number.
+--  select  from dual;
+-- Find greatest value among three numbers.
+-- select greatest(23,46,85) from dual;
+
+ -- Find least value among three numbers.
+-- select least(23,45,1) from dual;
+
+-- Calculate variance of sales.
+-- select variance(quantity* unit_price) from orders;
+
+-- Calculate standard deviation of sales.
+-- select stddev(quantity* unit_price) from orders;
+
+-- Find average deviation.
+-- Calculate geometric mean.
+-- Calculate harmonic mean.
+
+-- Find sum of squares.
+-- select sum(power(unit_price,2)) from orders;
+
+-- Calculate correlation between two columns.
+-- select variance(quantity* unit_price) from orders;
+
+-- Calculate regression slope.
